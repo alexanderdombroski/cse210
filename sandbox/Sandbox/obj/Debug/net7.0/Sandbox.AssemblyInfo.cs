1d72a685cba5ee56efa5c71d8ee05f1bda246fca
alexanderdombroski <dom21013@byui.edu>
2024-02-27 09:50:51 -0700

    updated readme

--- sandbox/Sandbox/obj/Debug/net7.0/Sandbox.AssemblyInfo.cs
+++ sandbox/Sandbox/obj/Debug/net7.0/Sandbox.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sandbox")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54d1579db809f665aee990120c8f577daa4634b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5048264d7459ba030cf9c48addc10da1a70e5ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sandbox")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sandbox")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
