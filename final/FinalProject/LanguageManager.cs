aaaef335e0e8816d2c6559ffc85ef87caeb5e58b
alexanderdombroski <dom21013@byui.edu>
2024-04-06 20:58:18 -0600

    Simplified Language Menu

--- final/FinalProject/LanguageManager.cs
+++ final/FinalProject/LanguageManager.cs
@@ -3,63 +3,61 @@ using System.Text.Json.Nodes;
 using System.Linq;
 public class LanguageManager : SettingsManager, MenuUtility.IMenu {
     // Attributes:
-    private Dictionary<string, List<string>> _filenames;
+    private Dictionary<string, List<string>> _languageData;
 
     // Constructors:
     public LanguageManager() {
         JsonObject filenames = JsonIO.DeserializeJsonObject("settings/languages.json");
-        _filenames = filenames.ToDictionary(kv => kv.Key, kv => kv.Value.AsArray().Select(array => array.ToString()).ToList());
+        _languageData = filenames.ToDictionary(kv => kv.Key, kv => kv.Value.AsArray().Select(array => array.ToString()).ToList());
     }
     // Methods:
-    public void DisplayLangugages() {
-        Console.Clear();
-        _filenames.ToList().ForEach(kv => Console.WriteLine($" > {kv.Key} - {kv.Value[0]} - .{kv.Value[1]}"));
-        int pauseTime = int.Min(int.Max(_filenames.Count, 2), 10) * 1000;
-        ConsoleUtility.PauseMiliseconds(pauseTime);
-    }
     private void SelectLanguage() {
-        var KeyValueList = _filenames.ToList();
+        var KeyValueList = _languageData.ToList();
         int SelectionIndex = MenuUtility.DisplayMenu("Select a Language", KeyValueList.Select(kv => kv.Key).ToList());
         _settings["selected_language"] = KeyValueList[SelectionIndex - 1].Key;
         UpdateSettings();
     }
-    public void AddLanguage() {
+    private void AddLanguage() {
         Console.Write("What is the name of the language: ");
         string language = Console.ReadLine();
         Console.Write("Type the name of the file to store snippets (ie css.json): ");
         string filename = Console.ReadLine();
         Console.Write("What is the extension of this language: ");
         string extension = Console.ReadLine();
-        _filenames.Add(language, new List<string>{filename, extension});
+        _languageData.Add(language, new List<string>{filename, extension});
+        SaveLanguages();
     }
-    public void RemoveLanguage() {
-        var KeyValueList = _filenames.ToList();
-        int RemoveIndex = MenuUtility.DisplayMenu("Remove a Language from manager (this will not delete the file)", KeyValueList.Select(kv => kv.Key).ToList());
+    private void RemoveLanguage() {
+        var KeyValueList = _languageData.ToList();
+        int RemoveIndex = MenuUtility.DisplayMenu("Remove a Language from manager (this will also delete the snippet file)", KeyValueList.Select(kv => kv.Key).ToList());
+        string filePath = _settings["snippets_path"] + _languageData[KeyValueList[RemoveIndex - 1].Key][0];
+        if (File.Exists(filePath)) {
+            File.Delete(filePath);
+        }
         KeyValueList.RemoveAt(RemoveIndex - 1);
-        _filenames = KeyValueList.ToDictionary(kv => kv.Key, kv => kv.Value);
+        _languageData = KeyValueList.ToDictionary(kv => kv.Key, kv => kv.Value);
+        SaveLanguages();
     }
-    public void SaveLanguages() {
+    private void SaveLanguages() {
         JsonObject saveData = new();
-        _filenames.ToList().ForEach(kv => saveData.Add(kv.Key, JsonValue.Create(kv.Value)));        
+        _languageData.ToList().ForEach(kv => saveData.Add(kv.Key, JsonValue.Create(kv.Value)));        
         JsonIO.SerializeJsonObject("settings/languages.json", saveData);
+        Console.Write("Languages Updated ");
+        ConsoleUtility.PauseMiliseconds(1000);
     } 
     public new void RunMenu() {
         MenuUtility.RunMenu(
             "Language Settings:",
             new List<string> {
-                "Display Languages",
                 "Select Language",
                 "Add Language",
                 "Remove Language",
-                "Update Language File",
                 "Return to Settings Menu"
             },
             new List<Action> {
-                DisplayLangugages,
                 SelectLanguage,
                 AddLanguage,
-                RemoveLanguage,
-                SaveLanguages
+                RemoveLanguage
             }
         );
     }
