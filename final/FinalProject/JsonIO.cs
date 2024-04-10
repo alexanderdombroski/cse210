27519d5c3dfa00274df8c9da243dcdfe598563db
alexanderdombroski <dom21013@byui.edu>
2024-04-09 20:43:33 -0600

    Added Json Conversion functions

--- final/FinalProject/JsonIO.cs
+++ final/FinalProject/JsonIO.cs
@@ -27,4 +27,23 @@ public static class JsonIO {
         // Creates an empty json object in a file.
         File.WriteAllText(filename, "{}");
     }
+    public static Dictionary<string, string> ReadJsonStringObject(string filename) {
+        JsonObject jsonObject = DeserializeJsonObject(filename);
+        return jsonObject.ToDictionary(kv => kv.Key, kv => kv.Value.ToString());
+    }
+    public static Dictionary<string, List<string>> ReadJsonListObject(string filename) {
+        JsonObject filenames = DeserializeJsonObject(filename);
+        return filenames.ToDictionary(kv => kv.Key, kv => kv.Value.AsArray().Select(array => array.ToString()).ToList());
+    }
+    public static JsonObject DictToObject(Dictionary<string, string> inputDict) {
+        JsonObject jsonObject = new();
+        inputDict.ToList().ForEach(kv => jsonObject.Add(kv.Key, kv.Value));        
+        return jsonObject;
+    }
+    public static JsonObject DictToObject(Dictionary<string, List<string>> inputDict) {        
+        // #MethodOverloadingIsAwesome
+        JsonObject jsonObject = new();
+        inputDict.ToList().ForEach(kv => jsonObject.Add(kv.Key, JsonValue.Create(kv.Value)));        
+        return jsonObject;
+    }
 }
